6c7b82b1b52000f569294bbceb03cb2a7edfa8f3
user <shafiqrasool104@gmail.com>
2023-05-31 01:36:00 +0500

    Added Login signup page

--- app/src/main/java/com/riphah/food/ui/LoginActivity.kt
+++ app/src/main/java/com/riphah/food/ui/LoginActivity.kt
@@ -2,11 +2,19 @@ package com.riphah.food.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 import com.riphah.food.R
+import com.riphah.food.ui.authntication.SignupFragment
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_signup)
+        setContentView(R.layout.activity_login)
+        supportFragmentManager.commit {
+            add(R.id.Fragment_container_login, SignupFragment())
+            addToBackStack(null)
+
+        }
     }
 }
--- app/src/main/java/com/riphah/food/ui/authntication/SignupFragment.kt
+++ app/src/main/java/com/riphah/food/ui/authntication/SignupFragment.kt
@@ -1,28 +1,113 @@
 package com.riphah.food.ui.authntication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.riphah.food.R
+import com.riphah.food.databinding.FragmentSignupBinding
+import com.riphah.food.model.User
+import com.riphah.food.showToast
 
 class SignupFragment : Fragment() {
-
+    private lateinit var binding:FragmentSignupBinding
+    val firebaseAuth:FirebaseAuth=FirebaseAuth.getInstance()
+    val firebaseUser:FirebaseUser?=firebaseAuth.currentUser
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_signup,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpListeners(view)
+
 
     }
+
+    private fun setUpListeners(view: View) {
+        binding.textViewToLogin.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.Fragment_container_login,LoginFragment())
+                addToBackStack(null)
+            }
+        }
+        binding.buttonCreateAccount.setOnClickListener {
+            val name:String=binding.UserNameEditText.text.toString().trim()
+            val email:String=binding.UserEmailEditText.text.toString().trim()
+            val password:String=binding.UserPasswordEditText.toString().trim()
+
+            if(name.isNotEmpty()){
+
+                binding.UserNameLayout.error=null
+
+                if(email.isNotEmpty()){
+
+                    binding.UserEmailLayout.error=null
+
+                    if(password.isNotEmpty()){
+
+                        binding.UserPasswordLayout.error=null
+
+                        val user=User(name,email,password)
+                        createUserAccount(user,view)
+
+                    }else{
+                        binding.UserPasswordLayout.error="PLease enter a password"
+                    }
+                }else{
+                    binding.UserEmailLayout.error="Please enter email"
+                }
+            }else{
+               binding.UserNameLayout.error="Please enter full name"
+
+            }
+        }
+    }
+
+    private fun createUserAccount(user: User,view: View) {
+        firebaseAuth.createUserWithEmailAndPassword(user.email,user.password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(view.context, "Account Created Successfully", Toast.LENGTH_SHORT)
+                    .show()
+
+                        } else {
+                            Toast.makeText(
+                                view.context,
+                                "Something went wrong!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+
+
 
     companion object {
 
     }
+    private fun sendEmailVerification(email:String) {
+        firebaseUser?.let {
+            it.sendEmailVerification().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    showToast("email sent to $email ")
+                }
+            }
+        }
+    }
 }
+
+
